28cd4c4b24a361196a1f6714158e4106df236edc
Peter Keenan <peter.keenan@sage.com>
2021-07-21 16:28:50 +0100

    Deploy HandleUserInput

--- HandleUserInput/src/HandleUserInput/Function.cs
+++ HandleUserInput/src/HandleUserInput/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -9,7 +10,9 @@ namespace HandleUserInput
     {
         public ProducedOutput FunctionHandler(ReceivedInput userInput, ILambdaContext context)
         {
+            LambdaLogger.Log($"\n*** {DateTime.Now} ***\nProcessing message object from input\n");
             var HandledInput = new ProducedOutput { FirstName = userInput.FirstName, Surname = userInput.Surname, Message = userInput.Message, ProcessedAt = System.DateTime.Now };
+            LambdaLogger.Log($"\n*** {DateTime.Now} ***\nSuccess: Message created!\n");
             return HandledInput;
         }
     }
